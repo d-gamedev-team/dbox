870a045943f6453b8afd92cdc648fb0d823f5afb
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2014-05-21 10:57:51 +0200

    Cleanup b2draw.

--- src/dbox/common/b2draw.d
+++ src/dbox/common/b2draw.d
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2011 Erin Catto http://box2d.org
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 1. The origin of this software must not be misrepresented; you must not
+ * claim that you wrote the original software. If you use this software
+ * in a product, an acknowledgment in the product documentation would be
+ * appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ * misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
 module dbox.common.b2draw;
 
 import core.stdc.float_;
@@ -6,10 +23,6 @@ import core.stdc.string;
 
 import dbox.common;
 import dbox.common.b2math;
-
-
-import dbox.common.b2math;
-import dbox.common;
 
 /// Color for debug drawing. Each value has the range [0,1].
 struct b2Color
@@ -29,6 +42,15 @@ struct b2Color
 /// entities in your game.
 class b2Draw
 {
+    enum
+    {
+        e_shapeBit = 0x0001,                                    ///< draw shapes
+        e_jointBit = 0x0002,                                    ///< draw joint connections
+        e_aabbBit = 0x0004,                                     ///< draw axis aligned bounding boxes
+        e_pairBit = 0x0008,                                     ///< draw broad-phase pairs
+        e_centerOfMassBit = 0x0010                              ///< draw center of mass frame
+    }
+
     /// Set the drawing flags.
     void SetFlags(uint32 flags)
     {
@@ -53,15 +75,6 @@ class b2Draw
         m_drawFlags &= ~flags;
     }
 
-    enum
-    {
-        e_shapeBit = 0x0001,                                    ///< draw shapes
-        e_jointBit = 0x0002,                                    ///< draw joint connections
-        e_aabbBit = 0x0004,                                     ///< draw axis aligned bounding boxes
-        e_pairBit = 0x0008,                                     ///< draw broad-phase pairs
-        e_centerOfMassBit = 0x0010                              ///< draw center of mass frame
-    }
-
     /// Draw a closed polygon provided in CCW order.
     abstract void DrawPolygon(const(b2Vec2)* vertices, int32 vertexCount, b2Color color);
 
@@ -81,5 +94,6 @@ class b2Draw
     /// @param xf a transform.
     abstract void DrawTransform(b2Transform xf);
 
+protected:
     uint32 m_drawFlags;
 }
