e75cffc59bd7210b0e61e9af800edd1ea8506f61
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2014-05-21 12:36:16 +0200

    Cleanup b2circlecontact.

--- src/dbox/dynamics/contacts/b2circlecontact.d
+++ src/dbox/dynamics/contacts/b2circlecontact.d
@@ -1,31 +1,50 @@
+/*
+ * Copyright (c) 2006-2009 Erin Catto http://www.box2d.org
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 1. The origin of this software must not be misrepresented; you must not
+ * claim that you wrote the original software. If you use this software
+ * in a product, an acknowledgment in the product documentation would be
+ * appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ * misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
 module dbox.dynamics.contacts.b2circlecontact;
 
 import core.stdc.float_;
 import core.stdc.stdlib;
 import core.stdc.string;
 
-import dbox.common;
-import dbox.collision.shapes;
-import dbox.common.b2math;
 import dbox.collision;
+import dbox.collision.shapes;
+import dbox.common;
 import dbox.dynamics;
+import dbox.dynamics.contacts;
 
-import dbox.dynamics.contacts.b2contact;
-
+///
 class b2CircleContact : b2Contact
 {
+    ///
     static b2Contact Create(b2Fixture* fixtureA, int32, b2Fixture* fixtureB, int32, b2BlockAllocator* allocator)
     {
         void* mem = allocator.Allocate(b2memSizeOf!b2CircleContact);
         return b2emplace!b2CircleContact(mem, fixtureA, fixtureB);
     }
 
+    ///
     static void Destroy(b2Contact contact, b2BlockAllocator* allocator)
     {
         destroy(contact);
         allocator.Free(cast(void*)contact, b2memSizeOf!b2CircleContact);
     }
 
+    ///
     this(b2Fixture* fixtureA, b2Fixture* fixtureB)
     {
         super(fixtureA, 0, fixtureB, 0);
@@ -33,6 +52,7 @@ class b2CircleContact : b2Contact
         assert(m_fixtureB.GetType() == b2Shape.e_circle);
     }
 
+    ///
     override void Evaluate(b2Manifold* manifold, b2Transform xfA, b2Transform xfB)
     {
         b2CollideCircles(manifold,
@@ -40,10 +60,3 @@ class b2CircleContact : b2Contact
                          cast(b2CircleShape)m_fixtureB.GetShape(), xfB);
     }
 }
-
-import dbox.dynamics.contacts.b2circlecontact;
-import dbox.dynamics.b2body;
-import dbox.dynamics.b2fixture;
-import dbox.dynamics.b2worldcallbacks;
-import dbox.common.b2blockallocator;
-import dbox.collision.b2timeofimpact;
