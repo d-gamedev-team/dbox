7d525593ee32ff16585d8a53c63ef1d2f1424977
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2014-05-21 11:52:00 +0200

    Cleanup b2timer.

--- src/dbox/common/b2timer.d
+++ src/dbox/common/b2timer.d
@@ -1,25 +1,44 @@
+/*
+ * Copyright (c) 2011 Erin Catto http://box2d.org
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 1. The origin of this software must not be misrepresented; you must not
+ * claim that you wrote the original software. If you use this software
+ * in a product, an acknowledgment in the product documentation would be
+ * appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ * misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
 module dbox.common.b2timer;
 
-import core.stdc.float_;
-import core.stdc.stdlib;
-import core.stdc.string;
-
 import dbox.common;
-import dbox.common.b2math;
-
 
 import std.datetime;
 
-import dbox.common;
-
 /// Workaround for missing default struct ctors in D.
-@property B2Timer b2Timer() { return B2Timer(1); }
+@property B2Timer b2Timer()
+{
+    return B2Timer(1);
+}
 
 /// Timer for profiling. This has platform specific code and may
 /// not work on every platform.
 struct B2Timer
 {
-    /// Constructor
+    /// This struct must be properly initialized with an explicit constructor.
+    @disable this();
+
+    /// This struct cannot be copied.
+    @disable this(this);
+
+    /// Explicit constructor.
+    /// Start the timer.
     this(int)
     {
         sw.start();
@@ -37,7 +56,7 @@ struct B2Timer
         return sw.peek.to!("msecs", float);
     }
 
-/* private */
+private:
 
     StopWatch sw;
 }
