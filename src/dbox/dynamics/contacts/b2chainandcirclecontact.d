ab3e9f4849fdeb8db0338ec50fd074df5c3c777c
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2014-05-21 12:03:51 +0200

    Cleanup b2chainandcirclecontact.

--- src/dbox/dynamics/contacts/b2chainandcirclecontact.d
+++ src/dbox/dynamics/contacts/b2chainandcirclecontact.d
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2006-2009 Erin Catto http://www.box2d.org
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 1. The origin of this software must not be misrepresented; you must not
+ * claim that you wrote the original software. If you use this software
+ * in a product, an acknowledgment in the product documentation would be
+ * appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ * misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
 module dbox.dynamics.contacts.b2chainandcirclecontact;
 
 import core.stdc.float_;
@@ -5,19 +22,17 @@ import core.stdc.stdlib;
 import core.stdc.string;
 
 import dbox.common;
-import dbox.collision.shapes;
-import dbox.common.b2math;
-import dbox.dynamics;
 import dbox.collision;
-
-import dbox.dynamics.contacts.b2contact;
+import dbox.collision.shapes;
+import dbox.dynamics;
+import dbox.dynamics.contacts;
 
 class b2ChainAndCircleContact : b2Contact
 {
     static b2Contact Create(b2Fixture* fixtureA, int32 indexA, b2Fixture* fixtureB, int32 indexB, b2BlockAllocator* allocator)
     {
         void* mem = allocator.Allocate(b2memSizeOf!b2ChainAndCircleContact);
-        return b2emplace!(b2ChainAndCircleContact)(mem, fixtureA, indexA, fixtureB, indexB);
+        return b2emplace!b2ChainAndCircleContact(mem, fixtureA, indexA, fixtureB, indexB);
     }
 
     static void Destroy(b2Contact contact, b2BlockAllocator* allocator)
@@ -45,10 +60,3 @@ class b2ChainAndCircleContact : b2Contact
                                cast(b2CircleShape)m_fixtureB.GetShape(), xfB);
     }
 }
-
-import dbox.dynamics.contacts.b2chainandcirclecontact;
-import dbox.common.b2blockallocator;
-import dbox.dynamics.b2fixture;
-import dbox.collision.shapes.b2chainshape;
-import dbox.collision.shapes.b2edgeshape;
-
