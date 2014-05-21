84285e0310d168e692b2091f0977b3bff6de6acb
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2014-05-21 12:57:12 +0200

    Cleanup b2contact.

--- src/dbox/dynamics/contacts/b2contact.d
+++ src/dbox/dynamics/contacts/b2contact.d
@@ -1,15 +1,3 @@
-module dbox.dynamics.contacts.b2contact;
-
-import core.stdc.float_;
-import core.stdc.stdlib;
-import core.stdc.string;
-
-import dbox.common;
-import dbox.collision.shapes;
-import dbox.common.b2math;
-import dbox.dynamics;
-import dbox.collision;
-
 /*
  * Copyright (c) 2006-2009 Erin Catto http://www.box2d.org
  *
@@ -27,14 +15,17 @@ import dbox.collision;
  * misrepresented as being the original software.
  * 3. This notice may not be removed or altered from any source distribution.
  */
+module dbox.dynamics.contacts.b2contact;
 
-// #ifndef B2_CONTACT_H
-// #define B2_CONTACT_H
+import core.stdc.float_;
+import core.stdc.stdlib;
+import core.stdc.string;
 
-import dbox.common.b2math;
-import dbox.collision.b2collision;
-import dbox.collision.shapes.b2shape;
-import dbox.dynamics.b2fixture;
+import dbox.collision;
+import dbox.collision.shapes;
+import dbox.common;
+import dbox.dynamics;
+import dbox.dynamics.contacts;
 
 /// Friction mixing law. The idea is to allow either fixture to drive the restitution to zero.
 /// For example, anything slides on ice.
@@ -53,6 +44,7 @@ float32 b2MixRestitution(float32 restitution1, float32 restitution2)
 alias b2ContactCreateFcn = b2Contact function(b2Fixture* fixtureA, int32 indexA,
                                        b2Fixture* fixtureB, int32 indexB,
                                        b2BlockAllocator* allocator);
+
 alias b2ContactDestroyFcn = void function(b2Contact contact, b2BlockAllocator* allocator);
 
 struct b2ContactRegister
@@ -80,86 +72,138 @@ struct b2ContactEdge
 /// that has no contact points.
 class b2Contact
 {
-    shared static this()
+    /// Get the contact manifold. Do not modify the manifold unless you understand the
+    /// internals of Box2D.
+    inout(b2Manifold)* GetManifold() inout
     {
-        b2_defaultFilter = new b2ContactFilter();
-        b2_defaultListener = new b2ContactListener();
-
-        AddType(&b2CircleContact.Create, &b2CircleContact.Destroy, b2Shape.e_circle, b2Shape.e_circle);
-        AddType(&b2PolygonAndCircleContact.Create, &b2PolygonAndCircleContact.Destroy, b2Shape.e_polygon, b2Shape.e_circle);
-        AddType(&b2PolygonContact.Create, &b2PolygonContact.Destroy, b2Shape.e_polygon, b2Shape.e_polygon);
-        AddType(&b2EdgeAndCircleContact.Create, &b2EdgeAndCircleContact.Destroy, b2Shape.e_edge, b2Shape.e_circle);
-        AddType(&b2EdgeAndPolygonContact.Create, &b2EdgeAndPolygonContact.Destroy, b2Shape.e_edge, b2Shape.e_polygon);
-        AddType(&b2ChainAndCircleContact.Create, &b2ChainAndCircleContact.Destroy, b2Shape.e_chain, b2Shape.e_circle);
-        AddType(&b2ChainAndPolygonContact.Create, &b2ChainAndPolygonContact.Destroy, b2Shape.e_chain, b2Shape.e_polygon);
+        return &m_manifold;
     }
 
-    static void AddType(b2ContactCreateFcn createFcn, b2ContactDestroyFcn destroyFcn, b2Shape.Type type1, b2Shape.Type type2)
+    /// Get the world manifold.
+    void GetWorldManifold(b2WorldManifold* worldManifold) const
     {
-        assert(0 <= type1 && type1 < b2Shape.e_typeCount);
-        assert(0 <= type2 && type2 < b2Shape.e_typeCount);
+        const(b2Body*) body_A  = m_fixtureA.GetBody();
+        const(b2Body*) body_B  = m_fixtureB.GetBody();
+        const(b2Shape) shapeA = m_fixtureA.GetShape();
+        const(b2Shape) shapeB = m_fixtureB.GetShape();
 
-        s_registers[type1][type2].createFcn  = createFcn;
-        s_registers[type1][type2].destroyFcn = destroyFcn;
-        s_registers[type1][type2].primary    = true;
-
-        if (type1 != type2)
-        {
-            s_registers[type2][type1].createFcn  = createFcn;
-            s_registers[type2][type1].destroyFcn = destroyFcn;
-            s_registers[type2][type1].primary    = false;
-        }
+        worldManifold.Initialize(&m_manifold, body_A.GetTransform(), shapeA.m_radius, body_B.GetTransform(), shapeB.m_radius);
     }
 
-    static b2Contact Create(b2Fixture* fixtureA, int32 indexA, b2Fixture* fixtureB, int32 indexB, b2BlockAllocator* allocator)
+    /// Is this contact touching?
+    bool IsTouching() const
     {
-        b2Shape.Type type1 = fixtureA.GetType();
-        b2Shape.Type type2 = fixtureB.GetType();
+        return (m_flags & e_touchingFlag) == e_touchingFlag;
+    }
 
-        assert(0 <= type1 && type1 < b2Shape.e_typeCount);
-        assert(0 <= type2 && type2 < b2Shape.e_typeCount);
+    /// Has this contact been disabled?
+    bool IsEnabled() const
+    {
+        return (m_flags & e_enabledFlag) == e_enabledFlag;
+    }
 
-        b2ContactCreateFcn createFcn = s_registers[type1][type2].createFcn;
-
-        if (createFcn)
+    /// Enable/disable this contact. This can be used inside the pre-solve
+    /// contact listener. The contact is only disabled for the current
+    /// time step (or sub-step in continuous collisions).
+    void SetEnabled(bool flag)
+    {
+        if (flag)
         {
-            if (s_registers[type1][type2].primary)
-            {
-                return createFcn(fixtureA, indexA, fixtureB, indexB, allocator);
-            }
-            else
-            {
-                return createFcn(fixtureB, indexB, fixtureA, indexA, allocator);
-            }
+            m_flags |= e_enabledFlag;
         }
         else
         {
-            return null;
+            m_flags &= ~e_enabledFlag;
         }
     }
 
-    static void Destroy(b2Contact contact, b2BlockAllocator* allocator)
+    /// Get the next contact in the world's contact list.
+    inout(b2Contact) GetNext() inout
     {
-        b2Fixture* fixtureA = contact.m_fixtureA;
-        b2Fixture* fixtureB = contact.m_fixtureB;
-
-        if (contact.m_manifold.pointCount > 0 &&
-            fixtureA.IsSensor() == false &&
-            fixtureB.IsSensor() == false)
-        {
-            fixtureA.GetBody().SetAwake(true);
-            fixtureB.GetBody().SetAwake(true);
-        }
-
-        b2Shape.Type typeA = fixtureA.GetType();
-        b2Shape.Type typeB = fixtureB.GetType();
-
-        assert(0 <= typeA && typeB < b2Shape.e_typeCount);
-        assert(0 <= typeA && typeB < b2Shape.e_typeCount);
-
-        b2ContactDestroyFcn destroyFcn = s_registers[typeA][typeB].destroyFcn;
-        destroyFcn(contact, allocator);
+        return m_next;
     }
+
+    /// Get fixture A in this contact.
+    inout(b2Fixture)* GetFixtureA() inout
+    {
+        return m_fixtureA;
+    }
+
+    /// Get the child primitive index for fixture A.
+    int32 GetChildIndexA() const
+    {
+        return m_indexA;
+    }
+
+    /// Get fixture B in this contact.
+    inout(b2Fixture)* GetFixtureB() inout
+    {
+        return m_fixtureB;
+    }
+
+    /// Get the child primitive index for fixture B.
+    int32 GetChildIndexB() const
+    {
+        return m_indexB;
+    }
+
+    /// Get the friction.
+    float32 GetFriction() const
+    {
+        return m_friction;
+    }
+
+    /// Override the default friction mixture. You can call this in b2ContactListener::PreSolve.
+    /// This value persists until set or reset.
+    void SetFriction(float32 friction)
+    {
+        m_friction = friction;
+    }
+
+    /// Reset the friction mixture to the default value.
+    void ResetFriction()
+    {
+        m_friction = b2MixFriction(m_fixtureA.m_friction, m_fixtureB.m_friction);
+    }
+
+    /// Get the restitution.
+    float32 GetRestitution() const
+    {
+        return m_restitution;
+    }
+
+    /// Override the default restitution mixture. You can call this in b2ContactListener::PreSolve.
+    /// The value persists until you set or reset.
+    void SetRestitution(float32 restitution)
+    {
+        m_restitution = restitution;
+    }
+
+    /// Reset the restitution to the default value.
+    void ResetRestitution()
+    {
+        m_restitution = b2MixRestitution(m_fixtureA.m_restitution, m_fixtureB.m_restitution);
+    }
+
+    /// Get the desired tangent speed. In meters per second.
+    float32 GetTangentSpeed() const
+    {
+        return m_tangentSpeed;
+    }
+
+    /// Set the desired tangent speed for a conveyor belt behavior. In meters per second.
+    void SetTangentSpeed(float32 speed)
+    {
+        m_tangentSpeed = speed;
+    }
+
+    /// Evaluate this contact with your own manifold and transforms.
+    abstract void Evaluate(b2Manifold* manifold, b2Transform xfA, b2Transform xfB);
+
+// note: this should be package but D access implementation is lacking.
+// do not use in user code.
+/* package: */
+public:
 
     this(b2Fixture* fA, int32 indexA, b2Fixture* fB, int32 indexB)
     {
@@ -192,6 +236,33 @@ class b2Contact
         m_restitution = b2MixRestitution(m_fixtureA.m_restitution, m_fixtureB.m_restitution);
 
         m_tangentSpeed = 0.0f;
+    }
+
+    // Flags stored in m_flags
+    enum
+    {
+        // Used when crawling contact graph when forming islands.
+        e_islandFlag = 0x0001,
+
+        // Set when the shapes are touching.
+        e_touchingFlag = 0x0002,
+
+        // This contact can be disabled (by user)
+        e_enabledFlag = 0x0004,
+
+        // This contact needs filtering because a fixture filter was changed.
+        e_filterFlag = 0x0008,
+
+        // This bullet contact had a TOI event
+        e_bulletHitFlag = 0x0010,
+
+        // This contact has a valid TOI in m_toi
+        e_toiFlag = 0x0020
+    }
+
+    void FlagForFiltering()
+    {
+        m_flags |= e_filterFlag;
     }
 
     // Update the contact manifold and touching status.
@@ -284,155 +355,85 @@ class b2Contact
         }
     }
 
-    b2Manifold* GetManifold()
+    static void AddType(b2ContactCreateFcn createFcn, b2ContactDestroyFcn destroyFcn, b2Shape.Type type1, b2Shape.Type type2)
     {
-        return &m_manifold;
-    }
+        assert(0 <= type1 && type1 < b2Shape.e_typeCount);
+        assert(0 <= type2 && type2 < b2Shape.e_typeCount);
 
-    const(b2Manifold)* GetManifold() const
-    {
-        return &m_manifold;
-    }
+        s_registers[type1][type2].createFcn  = createFcn;
+        s_registers[type1][type2].destroyFcn = destroyFcn;
+        s_registers[type1][type2].primary    = true;
 
-    void GetWorldManifold(b2WorldManifold* worldManifold) const
-    {
-        const(b2Body*) body_A  = m_fixtureA.GetBody();
-        const(b2Body*) body_B  = m_fixtureB.GetBody();
-        const(b2Shape) shapeA = m_fixtureA.GetShape();
-        const(b2Shape) shapeB = m_fixtureB.GetShape();
-
-        worldManifold.Initialize(&m_manifold, body_A.GetTransform(), shapeA.m_radius, body_B.GetTransform(), shapeB.m_radius);
-    }
-
-    void SetEnabled(bool flag)
-    {
-        if (flag)
+        if (type1 != type2)
         {
-            m_flags |= e_enabledFlag;
+            s_registers[type2][type1].createFcn  = createFcn;
+            s_registers[type2][type1].destroyFcn = destroyFcn;
+            s_registers[type2][type1].primary    = false;
+        }
+    }
+
+    static b2Contact Create(b2Fixture* fixtureA, int32 indexA, b2Fixture* fixtureB, int32 indexB, b2BlockAllocator* allocator)
+    {
+        b2Shape.Type type1 = fixtureA.GetType();
+        b2Shape.Type type2 = fixtureB.GetType();
+
+        assert(0 <= type1 && type1 < b2Shape.e_typeCount);
+        assert(0 <= type2 && type2 < b2Shape.e_typeCount);
+
+        b2ContactCreateFcn createFcn = s_registers[type1][type2].createFcn;
+
+        if (createFcn)
+        {
+            if (s_registers[type1][type2].primary)
+            {
+                return createFcn(fixtureA, indexA, fixtureB, indexB, allocator);
+            }
+            else
+            {
+                return createFcn(fixtureB, indexB, fixtureA, indexA, allocator);
+            }
         }
         else
         {
-            m_flags &= ~e_enabledFlag;
+            return null;
         }
     }
 
-    bool IsEnabled() const
+    static void Destroy(b2Contact contact, b2BlockAllocator* allocator)
     {
-        return (m_flags & e_enabledFlag) == e_enabledFlag;
+        b2Fixture* fixtureA = contact.m_fixtureA;
+        b2Fixture* fixtureB = contact.m_fixtureB;
+
+        if (contact.m_manifold.pointCount > 0 &&
+            fixtureA.IsSensor() == false &&
+            fixtureB.IsSensor() == false)
+        {
+            fixtureA.GetBody().SetAwake(true);
+            fixtureB.GetBody().SetAwake(true);
+        }
+
+        b2Shape.Type typeA = fixtureA.GetType();
+        b2Shape.Type typeB = fixtureB.GetType();
+
+        assert(0 <= typeA && typeB < b2Shape.e_typeCount);
+        assert(0 <= typeA && typeB < b2Shape.e_typeCount);
+
+        b2ContactDestroyFcn destroyFcn = s_registers[typeA][typeB].destroyFcn;
+        destroyFcn(contact, allocator);
     }
 
-    bool IsTouching() const
+    shared static this()
     {
-        return (m_flags & e_touchingFlag) == e_touchingFlag;
-    }
+        b2_defaultFilter = new b2ContactFilter();
+        b2_defaultListener = new b2ContactListener();
 
-    b2Contact GetNext()
-    {
-        return m_next;
-    }
-
-    const(b2Contact) GetNext() const
-    {
-        return m_next;
-    }
-
-    b2Fixture* GetFixtureA()
-    {
-        return m_fixtureA;
-    }
-
-    const(b2Fixture)* GetFixtureA() const
-    {
-        return m_fixtureA;
-    }
-
-    b2Fixture* GetFixtureB()
-    {
-        return m_fixtureB;
-    }
-
-    int32 GetChildIndexA() const
-    {
-        return m_indexA;
-    }
-
-    const(b2Fixture)* GetFixtureB() const
-    {
-        return m_fixtureB;
-    }
-
-    int32 GetChildIndexB() const
-    {
-        return m_indexB;
-    }
-
-    void FlagForFiltering()
-    {
-        m_flags |= e_filterFlag;
-    }
-
-    void SetFriction(float32 friction)
-    {
-        m_friction = friction;
-    }
-
-    float32 GetFriction() const
-    {
-        return m_friction;
-    }
-
-    void ResetFriction()
-    {
-        m_friction = b2MixFriction(m_fixtureA.m_friction, m_fixtureB.m_friction);
-    }
-
-    void SetRestitution(float32 restitution)
-    {
-        m_restitution = restitution;
-    }
-
-    float32 GetRestitution() const
-    {
-        return m_restitution;
-    }
-
-    void ResetRestitution()
-    {
-        m_restitution = b2MixRestitution(m_fixtureA.m_restitution, m_fixtureB.m_restitution);
-    }
-
-    void SetTangentSpeed(float32 speed)
-    {
-        m_tangentSpeed = speed;
-    }
-
-    float32 GetTangentSpeed() const
-    {
-        return m_tangentSpeed;
-    }
-
-    abstract void Evaluate(b2Manifold* manifold, b2Transform xfA, b2Transform xfB);
-
-    // Flags stored in m_flags
-    enum
-    {
-        // Used when crawling contact graph when forming islands.
-        e_islandFlag = 0x0001,
-
-        // Set when the shapes are touching.
-        e_touchingFlag = 0x0002,
-
-        // This contact can be disabled (by user)
-        e_enabledFlag = 0x0004,
-
-        // This contact needs filtering because a fixture filter was changed.
-        e_filterFlag = 0x0008,
-
-        // This bullet contact had a TOI event
-        e_bulletHitFlag = 0x0010,
-
-        // This contact has a valid TOI in m_toi
-        e_toiFlag = 0x0020
+        AddType(&b2CircleContact.Create, &b2CircleContact.Destroy, b2Shape.e_circle, b2Shape.e_circle);
+        AddType(&b2PolygonAndCircleContact.Create, &b2PolygonAndCircleContact.Destroy, b2Shape.e_polygon, b2Shape.e_circle);
+        AddType(&b2PolygonContact.Create, &b2PolygonContact.Destroy, b2Shape.e_polygon, b2Shape.e_polygon);
+        AddType(&b2EdgeAndCircleContact.Create, &b2EdgeAndCircleContact.Destroy, b2Shape.e_edge, b2Shape.e_circle);
+        AddType(&b2EdgeAndPolygonContact.Create, &b2EdgeAndPolygonContact.Destroy, b2Shape.e_edge, b2Shape.e_polygon);
+        AddType(&b2ChainAndCircleContact.Create, &b2ChainAndCircleContact.Destroy, b2Shape.e_chain, b2Shape.e_circle);
+        AddType(&b2ChainAndPolygonContact.Create, &b2ChainAndPolygonContact.Destroy, b2Shape.e_chain, b2Shape.e_polygon);
     }
 
     static __gshared b2ContactRegister[b2Shape.Type.e_typeCount][b2Shape.Type.e_typeCount] s_registers;
@@ -463,21 +464,3 @@ class b2Contact
 
     float32 m_tangentSpeed = 0;
 }
-
-import dbox.dynamics.contacts.b2contact;
-import dbox.dynamics.contacts.b2circlecontact;
-import dbox.dynamics.contacts.b2polygonandcirclecontact;
-import dbox.dynamics.contacts.b2polygoncontact;
-import dbox.dynamics.contacts.b2edgeandcirclecontact;
-import dbox.dynamics.contacts.b2edgeandpolygoncontact;
-import dbox.dynamics.contacts.b2chainandcirclecontact;
-import dbox.dynamics.contacts.b2chainandpolygoncontact;
-import dbox.dynamics.contacts.b2contactsolver;
-
-import dbox.collision.b2collision;
-import dbox.collision.b2timeofimpact;
-import dbox.collision.shapes.b2shape;
-import dbox.common.b2blockallocator;
-import dbox.dynamics.b2body;
-import dbox.dynamics.b2fixture;
-import dbox.dynamics.b2world;
